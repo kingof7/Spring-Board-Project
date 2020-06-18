10625836130125e0904de12b1fb23c0aadaf1d31
kingof7 <vlfl0@naver.com>
2020-06-18 20:52:22 +0900

    adding paging and searching

---
+++ src/main/java/kr/co/vo/BoardVO.java
@@ -0,0 +1,43 @@
+package kr.co.vo;
+
+import java.util.Date;
+
+//vo는 테이블의 컬럼값들을 객체로 다루기 위한 것
+public class BoardVO {
+	private int bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regdate;
+	
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+}
