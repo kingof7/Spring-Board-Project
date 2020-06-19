9b7d258aff7b5acd398a204ec235da60bf25e7f5
kingof7 <vlfl0@naver.com>
2020-06-19 13:49:58 +0900

    remaining searching

--- src/main/java/kr/co/controller/BoardController.java
+++ src/main/java/kr/co/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kr.co.service.BoardService;
 import kr.co.vo.BoardVO;
@@ -60,10 +61,11 @@ public class BoardController {
 
 	// 게시글 조회
 	@RequestMapping(value = "/readView", method = RequestMethod.GET)
-	public String read(BoardVO boardVO, Model model) throws Exception {
+	public String read(BoardVO boardVO, @ModelAttribute("scri") SearchCriteria scri, Model model) throws Exception {
 		logger.info("read");
 
 		model.addAttribute("read", service.read(boardVO.getBno()));
+		model.addAttribute("scri", scri);
 		// 데이터가 담겨있는 객체
 
 		return "board/readView";
@@ -71,30 +73,44 @@ public class BoardController {
 
 	// 게시판 수정뷰
 	@RequestMapping(value = "/updateView", method = RequestMethod.GET)
-	public String updateView(BoardVO boardVO, Model model) throws Exception {
+	public String updateView(BoardVO boardVO, @ModelAttribute("scri") SearchCriteria scri, Model model)
+			throws Exception {
 		logger.info("updateView");
 
 		model.addAttribute("update", service.read(boardVO.getBno()));
+		model.addAttribute("scri", scri);
 
 		return "board/updateView";
 	}
 
-	// 게시판 수정->저장
+	// 게시판 수정
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public String update(BoardVO boardVO) throws Exception {
+	public String update(BoardVO boardVO, @ModelAttribute("scri") SearchCriteria scri, RedirectAttributes rttr)
+			throws Exception {
 		logger.info("update");
 
 		service.update(boardVO);
+
+		rttr.addAttribute("page", scri.getPage());
+		rttr.addAttribute("perPageNum", scri.getPerPageNum());
+		rttr.addAttribute("searchType", scri.getSearchType());
+		rttr.addAttribute("keyword", scri.getKeyword());
 
 		return "redirect:/board/list";
 	}
 
 	// 게시판 삭제
 	@RequestMapping(value = "/delete", method = RequestMethod.POST)
-	public String delete(BoardVO boardVO) throws Exception {
+	public String delete(BoardVO boardVO, @ModelAttribute("scri") SearchCriteria scri, RedirectAttributes rttr)
+			throws Exception {
 		logger.info("delete");
 
 		service.delete(boardVO.getBno());
+
+		rttr.addAttribute("page", scri.getPage());
+		rttr.addAttribute("perPageNum", scri.getPerPageNum());
+		rttr.addAttribute("searchType", scri.getSearchType());
+		rttr.addAttribute("keyword", scri.getKeyword());
 
 		return "redirect:/board/list";
 	}
--- src/main/webapp/WEB-INF/views/board/list.jsp
+++ src/main/webapp/WEB-INF/views/board/list.jsp
@@ -38,8 +38,8 @@ li {
 					<c:forEach items="${list}" var="list">
 						<tr>
 							<td><c:out value="${list.bno}" /></td>
-							<td><a href="/board/readView?bno=${list.bno}"><c:out
-										value="${list.title}" /></a></td>
+							<td><a href="/board/readView?bno=${list.bno}&page=${scri.page}&perPageNum=${scri.perPageNum}&searchType=${scri.searchType}&keyword=${scri.keyword}">
+							<c:out value="${list.title}" /></a></td>
 							<td><c:out value="${list.writer}" /></td>
 							<td><fmt:formatDate value="${list.regdate}"
 									pattern="yyyy-MM-dd" /></td>
@@ -58,7 +58,7 @@ li {
 				
 				    <input type="text" name="keyword" id="keywordInput" value="${scri.keyword}"/>
 				
-				    <button id="searchBtn" type="button">검색</button>
+				    <button id="searchBtn" type="submit">검색</button>
 				    <script>
 				      $(function(){
 				        $('#searchBtn').click(function() {
